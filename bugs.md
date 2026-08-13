5f7524507bb962b1e6df49e5c7459a12d058d939
Aayush Ray <aayushray2020@gmail.com>
2026-08-13 18:34:18 +0530

    doing some changes

---
+++ bugs.md
@@ -0,0 +1,4 @@
+#unique id everytime generated // done
+#listener's except //done
+#else : while True : pass for the starting peer issue 
+#handle peer never removes this things 
--- peer.py
+++ peer.py
@@ -17,10 +17,10 @@ def decd_msg(conn, n):
     return data
 
 def get_msg(conn):
-    head = decd_msg(conn, 4)
-    length = struct.unpack('!I', head)[0]
-    data = decd_msg(conn, length).decode('utf-8')
-    return json.loads(data)
+        head = decd_msg(conn, 4)
+        length = struct.unpack('!I', head)[0]
+        data = decd_msg(conn, length).decode('utf-8')
+        return json.loads(data)
 
 def encd_msg(msg):
     json_msg = json.dumps(msg)
@@ -29,41 +29,46 @@ def encd_msg(msg):
     return header + json_data
 
 def handle_peer(conn):
-    while True:
-        msg = get_msg(conn)
-        peer_name = msg.get("unique_id")
+        peer_name = None
+        try : 
+             while True:
 
-        if peer_name not in peers:
-            peers[peer_name] = conn
-        print(msg.get("content"))
-
+                msg = get_msg(conn)
+                peer_name = msg.get("unique_id")
+                name = msg.get("name")
+                if peer_name not in peers:
+                    peers[peer_name] = conn
+                print("Name",name,":",msg.get("content"))
+        except ConnectionError as e : 
+            print (f"Error : {e}")
+        finally:
+            if peer_name : 
+                peers.pop(peer_name,None)
+            conn.close()
         
 
-def send_conn(conn):
-    name = input("Enter name: ")
-    alpha = string.ascii_uppercase + string.digits
-    unique_id = ''.join(secrets.choice(alpha) for _ in range(6))
+def send_conn(conn,name,unique_id):
     msg = input("Enter the message: ")
     send = {"unique_id": unique_id, "name": name, "content": msg}
     conn.sendall(encd_msg(send))
 
 def listner(s):
-    try:
+   
         while True:
-            conn, addr = s.accept()
-            
-            t1 = threading.Thread(target=handle_peer, args=(conn,))
-            t1.daemon = True
-            t1.start()
-    except Exception as e:
-        print(f"Error : {e}")
+            try:
+                conn, addr = s.accept()
+                t1 = threading.Thread(target=handle_peer, args=(conn,))
+                t1.daemon = True
+                t1.start()
+            except Exception as e :
+                print(f"Error {e}")
 
 def connect(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, port))   
     return s
 
-def start_peer(my_port, peer_ip=None, peer_port=None):
+def start_peer(my_port, peer_ip=None, peer_port=None,Name=None,unique_id=None):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(('0.0.0.0', my_port))  
@@ -79,19 +84,22 @@ def start_peer(my_port, peer_ip=None, peer_port=None):
         t2.daemon = True
         t2.start()
         while True:
-         send_conn(conn)   
+         send_conn(conn,Name,unique_id)   
     else:
         while True:
             pass
 
 def peer():
+    name = input("Enter your name")
+    alpha = string.ascii_uppercase + string.ascii_letters
+    unique_id = ''.join(secrets.choice(alpha) for _ in range (6))
     my_port = int(input("Enter your port: "))
     join = input("Connect to existing peer (y/n)? ")
     if join == 'y':
         peer_ip = input("Peer IP: ")
         peer_port = int(input("Port: "))
-        start_peer(my_port, peer_ip, peer_port)
+        start_peer(my_port, peer_ip, peer_port,name,unique_id)
     else:
-        start_peer(my_port)
+        start_peer(my_port,Name = name,unique_id=unique_id)
 
 peer()
